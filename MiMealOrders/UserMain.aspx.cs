4c6b34cfec8753ae7bdaa042158c2a1addf89ab1
Scott Murphy <scottroymurphy@hotmail.com>
2026-04-09 13:37:01 -0500

    Add UserMain.aspx.cs

---
+++ MiMealOrders/UserMain.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiMealOrders
+{
+    public partial class MainOrder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+        }
+
+        protected void ddlStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
