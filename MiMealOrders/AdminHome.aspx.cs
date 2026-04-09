e95506ca698e22e5734a73118d13b3b26163d7cd
Scott Murphy <scottroymurphy@hotmail.com>
2026-04-09 13:36:54 -0500

    Add AdminHome.aspx.cs

---
+++ MiMealOrders/AdminHome.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiMealOrders
+{
+    public partial class AdminHome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btCampus_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MenuCreation.aspx");
+        }
+
+        protected void btVendor_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("VendorCreation.aspx");
+        }
+
+        protected void btMenu_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MenuCreation.aspx");
+        }
+
+        protected void btMenuItems_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MenuItemCreation.aspx");
+        }
+
+        protected void btMenuCreation_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MenuAdminView.aspx");
+        }
+
+        protected void btHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
