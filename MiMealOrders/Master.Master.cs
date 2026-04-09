2456ea50b43308dbe9d0d7feb801dd8c9daa7d3b
Scott Murphy <scottroymurphy@hotmail.com>
2026-04-09 13:36:45 -0500

    Add Master.Master.cs

---
+++ MiMealOrders/Master.Master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiMealOrders
+{
+    public partial class Master : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // if ((Request.Url.ToString().ToLower().IndexOf("Default.aspx") > -1) || (Request.Url.ToString().ToLower().IndexOf(".aspx") == -1) ) 
+           // {
+           //     pnMenu.Visible = false;
+           // }
+           // else
+           // {
+           //     pnMenu.Visible = true;
+           // }
+        }
+    }
+}
