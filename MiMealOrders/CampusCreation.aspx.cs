64b078578c3b3e6db9d0e0886570e061d7cd7fa9
Scott Murphy <scottroymurphy@hotmail.com>
2026-04-09 13:37:19 -0500

    Add CampusCreation.aspx.cs

---
+++ MiMealOrders/CampusCreation.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiMealOrders
+{
+    public partial class CampusCreation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminHome.aspx");
+        }
+
+        protected void btSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
